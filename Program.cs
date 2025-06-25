310af9df277a2ec35a4f754382feba39ad972ed4
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2025-06-25 18:15:30 +0200

    update

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static void Main()
+    {
+        var config = new ConfigurationBuilder().AddJsonFile("config.json").Build();
+
+        string? DbConnectionString = config.GetConnectionString("DbConnection");
+
+        //var dbSection = config.GetSection("DbConnection");
+        //string DbConnectionString = $"Server={dbSection["server"]};Database={dbSection["database"]};User Id={dbSection["userId"]};Password={dbSection["password"]};";
+
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(DbConnectionString))
+            {
+                conn.Open();
+                Console.WriteLine("Berhasil terhubung ke database.");
+
+                string query = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Console.WriteLine("Daftar tabel dalam database:");
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("- " + reader.GetString(0));
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Gagal terkoneksi atau error: " + ex.Message);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+
+config.json
+
+```json
+{
+    "ConnectionStrings": {
+        "DbConnection": "Server=___;Database=___;User Id=___;Password=___;"
+    }
+}
+
+/* {
+    "DbConnection": {
+        "server": "",
+        "database": "",
+        "userId": "",
+        "password": ""
+    }
+} */
+```
