a2bc4828aae5792d917812869de053a0ab12f3d8
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2025-06-26 02:26:08 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,34 +1,7 @@
-﻿using System;
-using System.Data.SqlClient;
-using csdb.Database;
+﻿/* using System; */
+using csdb.Utils;
 
-class Program {
-    static void ExecuteUserQuery(string query) {
-        try {
-            using (SqlConnection connection = DbConnector.CreateConnection()) {
-                connection.Open();
-                using (SqlCommand command = new SqlCommand(query, connection)) {
-                    if (query.TrimStart().StartsWith("SELECT", StringComparison.OrdinalIgnoreCase)) {
-                        using (SqlDataReader reader = command.ExecuteReader()) {
-                            int fieldCount = reader.FieldCount;
-                            while (reader.Read()) {
-                                for (int i = 0; i < fieldCount; i++) {
-                                    Console.Write(reader.GetName(i) + ": " + reader[i] + "\t");
-                                }
-                                Console.WriteLine();
-                            }
-                        }
-                    } else {
-                        int affected = command.ExecuteNonQuery();
-                        Console.WriteLine($"{affected} row(s) affected.");
-                    }
-                }
-            }
-        } catch (Exception ex) {
-            Console.WriteLine("Error: " + ex.Message);
-        }
-    }
-    
+class Program {    
     static void Main() {
         Console.WriteLine("== type '/exit' to exit ==");
         while (true) {
@@ -42,7 +15,8 @@ class Program {
             else if (userInput.Trim().ToLower() == "/table")
                 userInput = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'";
 
-            ExecuteUserQuery(userInput);
+            long? executionTime = Helper.ExecuteUserQuery(userInput);
+            Console.WriteLine($"Execution Time: {executionTime} ms");
         }
     }
 }
